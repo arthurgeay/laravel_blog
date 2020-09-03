9dd3362432bb5ac0cd1980f26c19dae422e7af1b
Arthur Geay <arthurgeay.contact@gmail.com>
2020-09-03 22:52:18 +0200

    :sparkles: Add Post model

---
+++ app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $name = 'Post';
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+}
