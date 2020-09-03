c5d20dcee0219060bd06dd10b083775058b4f241
Arthur Geay <arthurgeay.contact@gmail.com>
2020-09-03 19:21:35 +0200

    :seedling: Add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        DB::table('users')->delete();
+
+        for($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => 'test'.$i.'@example.com',
+                'password' => bcrypt('test')
+            ]);
+        }
+    }
+}
