316263e5821fbbed343f936904843c424f013db9
Arthur Geay <arthurgeay.contact@gmail.com>
2020-09-09 17:20:45 +0200

    :card_file_box: Add on delete cascade for comments where post is deleted

--- database/migrations/2020_09_07_154131_create_comments_table.php
+++ database/migrations/2020_09_07_154131_create_comments_table.php
@@ -23,6 +23,7 @@ class CreateCommentsTable extends Migration
             $table->foreign('post_id')
                 ->references('id')
                 ->on('posts')
+                ->onDelete('cascade')
                 ;
 
         });
