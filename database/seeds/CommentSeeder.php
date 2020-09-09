4d46554a70325815960b51c998e8a88c786b6670
Arthur Geay <arthurgeay.contact@gmail.com>
2020-09-09 17:51:46 +0200

    :seedling: Update CommentSeeder with random reports

--- database/seeds/CommentSeeder.php
+++ database/seeds/CommentSeeder.php
@@ -20,6 +20,7 @@ class CommentSeeder extends Seeder
                 'content' => $faker->text(40),
                 'created_at' => $faker->dateTime,
                 'post_id' => $faker->numberBetween(1, 20),
+                'reports' => $faker->boolean() ? $faker->numberBetween(1, 15) : null
             ]);
         }
     }
